6459f8d249d4254dbe8e3343280c343f10da89d9
thamodperera <thamodperera258@gmail.com>
2026-02-28 16:38:19 +0530

    no data

--- frontend/src/components/common/ProtectedRoute.jsx
+++ frontend/src/components/common/ProtectedRoute.jsx
@@ -1,18 +1,1 @@
-import { Navigate, useLocation } from "react-router-dom";
-
-export function ProtectedRoute({ children }) {
-  const token = localStorage.getItem("token");
-  const userRole = localStorage.getItem("role");
-  const location = useLocation();
-
-  if (!token) {
-    return <Navigate to="/login" state={{ from: location }} replace />;
-  }
-
-  // Prevent admin users from accessing customer routes
-  if (userRole === "ADMIN") {
-    return <Navigate to="/admin_dashboard" replace />;
-  }
-
-  return children;
-}
+to be added 
