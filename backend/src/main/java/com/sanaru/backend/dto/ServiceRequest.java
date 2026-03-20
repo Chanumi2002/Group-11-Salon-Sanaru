90df1b29959d7d43ee0875c22a3b57ebb7ef3952
thamodperera <thamodperera258@gmail.com>
2026-03-21 01:20:42 +0530

    feature/Admin-Manage-Services-(CRUD)

---
+++ backend/src/main/java/com/sanaru/backend/dto/ServiceRequest.java
@@ -0,0 +1,14 @@
+package com.sanaru.backend.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class ServiceRequest {
+    private String name;
+    private String description;
+    private BigDecimal price;
+}
